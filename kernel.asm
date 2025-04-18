eceef3bb273c0d1a785db668cf75a4dc9a2539cb
Loxsete <gduck3029@gmail.com>
2025-04-18 11:00:50 +0300

    Update kernel.asm
    What's added: switched to 0x12 graphics mode (640x480, 16 colors.) Added a program for dumping RAM. In the next update I'll make LoxFS (it will be a portable file system.)

--- kernel.asm
+++ kernel.asm
@@ -3,7 +3,7 @@
 
 kernel_start:
     mov ah, 0x00
-    mov al, 0x13
+    mov al, 0x12
     int 0x10
 
     mov si, info_msg1
@@ -20,6 +20,9 @@ input_start:
     mov si, prompt
     call print_string
 
+    mov bx, 0
+    mov dx, 3
+
 input_loop:
     mov ah, 0x00
     int 0x16
@@ -29,6 +32,12 @@ input_loop:
 
     cmp al, 0x08
     je handle_backspace
+
+    cmp al, 0x48
+    je handle_up_arrow
+
+    cmp al, 0x50
+    je handle_down_arrow
 
     mov ah, 0x0e
     mov bh, 0
@@ -47,7 +56,6 @@ handle_backspace:
     dec di
     dec cx
     mov byte [di], 0
-
     mov ah, 0x0e
     mov al, 0x08
     int 0x10
@@ -55,7 +63,18 @@ handle_backspace:
     int 0x10
     mov al, 0x08
     int 0x10
+    jmp input_loop
 
+handle_up_arrow:
+    cmp bx, 0
+    je input_loop
+    dec bx
+    jmp input_loop
+
+handle_down_arrow:
+    cmp bx, dx
+    je input_loop
+    inc bx
     jmp input_loop
 
 process_command:
@@ -63,26 +82,34 @@ process_command:
     call print_newline
 
     mov si, buffer
-
     mov di, cmd_clear
-    call strcmp
+    call strcmp_first_word
     jc do_clear
 
+    mov si, buffer
     mov di, cmd_cpuid
-    call strcmp
+    call strcmp_first_word
     jc do_cpuid
 
+    mov si, buffer
     mov di, cmd_help
-    call strcmp
+    call strcmp_first_word
     jc do_help
 
+    mov si, buffer
     mov di, cmd_info
-    call strcmp
+    call strcmp_first_word
     jc do_info
 
+    mov si, buffer
     mov di, cmd_mem
-    call strcmp
+    call strcmp_first_word
     jc do_mem
+
+    mov si, buffer
+    mov di, cmd_dump
+    call strcmp_first_word
+    jc do_dump
 
     mov si, error_msg
     call print_string
@@ -91,9 +118,163 @@ process_command:
 
 do_clear:
     mov ah, 0x00
-    mov al, 0x13
+    mov al, 0x12
     int 0x10
     jmp reset_buffer
+
+do_dump:
+    mov si, buffer
+    call skip_word
+    cmp byte [si], 0
+    je .use_default
+
+    call parse_hex
+    mov bx, ax
+    jmp .start_dump
+
+.use_default:
+    mov bx, 0
+
+.start_dump:
+    mov si, dump_header
+    call print_string
+    call print_newline
+
+    mov si, dump_address_msg
+    call print_string
+    mov ax, bx
+    call print_hex
+    call print_newline
+
+    mov cx, 16
+
+.dump_line:
+    push cx
+
+    mov ax, bx
+    call print_address
+
+    mov di, temp_buffer
+    mov cx, 16
+
+.dump_bytes:
+    mov al, [es:bx]
+
+    cmp al, 32
+    jl .non_printable
+    cmp al, 126
+    jg .non_printable
+    jmp .printable
+
+.non_printable:
+    mov byte [di], '.'
+    jmp .store_ascii
+
+.printable:
+    mov [di], al
+
+.store_ascii:
+    inc di
+
+    call print_hex
+    call print_space
+
+    inc bx
+    loop .dump_bytes
+
+    call print_space
+    call print_space
+    mov byte [di], 0
+    mov si, temp_buffer
+    call print_string
+
+    call print_newline
+
+    pop cx
+    dec cx
+    jnz .dump_line
+
+    call print_newline
+    jmp reset_buffer
+
+skip_word:
+    cmp byte [si], 0
+    je .done
+    cmp byte [si], ' '
+    je .skip_spaces
+    inc si
+    jmp skip_word
+
+.skip_spaces:
+    inc si
+    cmp byte [si], ' '
+    je .skip_spaces
+
+.done:
+    ret
+
+parse_hex:
+    xor ax, ax
+
+.parse_loop:
+    cmp byte [si], 0
+    je .done
+    cmp byte [si], ' '
+    je .done
+
+    shl ax, 4
+
+    mov bl, [si]
+
+    cmp bl, '0'
+    jl .error
+    cmp bl, '9'
+    jle .digit
+
+    cmp bl, 'A'
+    jl .error
+    cmp bl, 'F'
+    jle .upper_letter
+
+    cmp bl, 'a'
+    jl .error
+    cmp bl, 'f'
+    jle .lower_letter
+    jmp .error
+
+.digit:
+    sub bl, '0'
+    jmp .add_digit
+
+.upper_letter:
+    sub bl, 'A'
+    add bl, 10
+    jmp .add_digit
+
+.lower_letter:
+    sub bl, 'a'
+    add bl, 10
+
+.add_digit:
+    add al, bl
+    inc si
+    jmp .parse_loop
+
+.error:
+    xor ax, ax
+
+.done:
+    ret
+
+print_space:
+    push ax
+    mov al, ' '
+    mov ah, 0x0e
+    mov bx, 0
+    mov bl, 0x07
+    int 0x10
+    pop ax
+    ret
 
 do_cpuid:
     mov si, cpuid_msg
@@ -276,24 +457,64 @@ do_info:
     call print_string
     call print_newline
 
+    mov si, info_row10
+    call print_string
+    call print_newline
+
     mov si, info_border
     call print_string
     call print_newline
     jmp reset_buffer
 
 do_mem:
-    int 0x12
-    mov si, mem_msg
+    mov si, dump_header
     call print_string
+    call print_newline
 
-    mov bx, ax
-    mov al, bh
+    mov si, 0x0000
+    mov es, si
+    mov si, 0x0000
+    mov cx, 256
+    mov bx, 0
+
+.next_byte:
+    mov ax, bx
+    call print_address
+
+    mov al, [es:si]
     call print_hex
-    mov al, bl
-    call print_hex
+    mov al, ' '
+    call print_char
+
+    inc si
+    inc bx
+    loop .next_byte
 
     call print_newline
     jmp reset_buffer
+
+print_char:
+    push ax
+    push bx
+    mov ah, 0x0e
+    mov bh, 0
+    mov bl, 0x07
+    int 0x10
+    pop bx
+    pop ax
+    ret
+
+print_address:
+    push ax
+    push bx
+    call print_hex
+    mov al, ':'
+    call print_char
+    mov al, ' '
+    call print_char
+    pop bx
+    pop ax
+    ret
 
 reset_buffer:
     mov di, buffer
@@ -380,30 +601,71 @@ strcmp_equal:
     pop si
     ret
 
+strcmp_first_word:
+    push si
+    push di
+strcmp_first_loop:
+    mov al, [si]
+    mov bl, [di]
+
+    cmp bl, 0
+    jne .continue_compare
+
+    cmp al, 0
+    je strcmp_first_equal
+    cmp al, ' '
+    je strcmp_first_equal
+    jmp strcmp_first_not_equal
+
+.continue_compare:
+    cmp al, bl
+    jne strcmp_first_not_equal
+    cmp al, 0
+    je strcmp_first_equal
+    inc si
+    inc di
+    jmp strcmp_first_loop
+
+strcmp_first_not_equal:
+    clc
+    pop di
+    pop si
+    ret
+
+strcmp_first_equal:
+    stc
+    pop di
+    pop si
+    ret
+
 prompt     db '> ', 0
 error_msg  db 'Unknown command', 0
-help_msg   db 'Commands: clear, cpuid, help, info, mem', 0
+help_msg   db 'Commands: clear, cpuid, help, info, mem, dump', 0
+dump_address_msg   db 'Memory dump from address 0x', 0
 cpuid_msg  db 'CPU Info:', 0
-info_msg1  db 'Welcone to LoxOS 0.2! Thanks for testing', 0
+info_msg1  db 'Welcome to LoxOS 0.2! Thanks for testing', 0
 mem_msg    db 'RAM memory size: 0x', 0
-temp_buffer     times 64 db 0
-vendor_msg     db 'Vendor: ', 0
-name_msg       db 'CPU Name: ', 0
-threads_msg    db 'Logical Processors: ', 0
+temp_buffer times 64 db 0
+vendor_msg  db 'Vendor: ', 0
+name_msg    db 'CPU Name: ', 0
+threads_msg db 'Logical Processors: ', 0
 unknown_cores_msg db 'CPU cores: Cannot determine', 0
+dump_header db 'Dumping first 256 bytes of memory:', 0
 info_border db '+----------------------------------------+', 0
-info_row1   db '| LoxOS 0.1                             |', 0
+info_row1   db '| LoxOS 0.3                             |', 0
 info_row2   db '| Created by Loxsete                    |', 0
-info_row3   db '| Mode: 0x13 graphics                   |', 0
+info_row3   db '| Mode: 0x12 graphics                   |', 0
 info_row4   db '| Kernel: Mini, 16-bit                  |', 0
 info_row5   db '| Commands:                             |', 0
 info_row6   db '| help  - show help                     |', 0
 info_row7   db '| mem   - show RAM                      |', 0
 info_row8   db '| clear - clear screen                  |', 0
 info_row9   db '| cpuid - show CPU Info                 |', 0
-cmd_clear  db 'clear', 0
-cmd_cpuid  db 'cpuid', 0
-cmd_help   db 'help', 0
-cmd_info   db 'info', 0
-cmd_mem    db 'mem', 0
-buffer     times 256 db 0
+info_row10 db '| dump or dump XXX - dump memory        |', 0
+cmd_clear   db 'clear', 0
+cmd_cpuid   db 'cpuid', 0
+cmd_help    db 'help', 0
+cmd_info    db 'info', 0
+cmd_mem     db 'mem', 0
+cmd_dump    db 'dump', 0
+buffer      times 256 db 0
